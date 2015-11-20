a258bcc0937d39979fae33567659e9d7f771dde1
Warren Bailey <warren@methodsdigital.co.uk>
2015-11-20 08:45:12 +0000

    DIGEQ-94 Initial check in of performance test scripts

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # eq-performance-test
+
+##Install JMeter
+sudo apt-get install jmeter
+
+##Install Ruby JMeter
+
+sudo gem install ruby-jmeter
+
+##Generating the Jmeter jmx file
+
+cd dsl
+
+ruby digitaleq-jmx.rb
+
+##Executing on flood.io
+
+ruby digitaleq-flood.rb
---
+++ dsl/digitaleq.rb
@@ -0,0 +1,33 @@
+require 'rubygems'
+require 'ruby-jmeter'
+
+
+if ARGV.size != 3
+  puts "      Usage: ruby digitaleq <environment> <number_of_users> <duration>"
+  puts "      Please specify a target env, number of users and duration of test"
+  puts "      Example:"
+  puts "          ruby ruby digitaleq sprint5 10 180"
+  exit 1
+end
+
+environment  = ARGV[0]
+thread_count = ARGV[1].to_i
+duration   = ARGV[2].to_i
+
+puts "Executing tests on http://"+ environment +".survey.eq.ons.digital/questionnaire/1 with "+thread_count.to_s+" users for "+duration.to_s+" seconds "
+
+test do
+  threads count: thread_count, duration: duration, continue_forever: true do
+    visit name: 'Introduction', url: 'http://'+ environment +'.survey.eq.ons.digital/questionnaire/1'
+        extract css: 'a.hyphenate', name: 'location'
+    submit name: 'Question Page', url: '${location}',
+         fill_in: {
+            start: 'Proceed'
+         }
+    submit name: 'Complete Page', url: '${location}',
+         fill_in: {
+            EQ_0_1: 'Answer',
+            next: 'Save and continue'
+         }
+  end
+end.jmx
