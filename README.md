88f8ba06f5ad021f7d1bdc26f468388bbc7a0ff7
Chris Collis <collisdigital@users.noreply.github.com>
2016-07-22 16:28:18 +0100

    Switch to ruby-jmeter and update to work with latest Star Wars survey

--- README.md
+++ README.md
@@ -2,26 +2,89 @@
 
 ## Background
 
-This repo is a suite of performance tests to be run against the survey-runner component of the EQ platform using JMeter
+This repo is a suite of performance tests to be run against the survey-runner
+component of the EQ platform using JMeter. It uses ruby-jmeter to generate the
+Jmeter XML definition rather than editing the XML directly or via the GUI.
 
-## How to install
+## Installation
+It is recommended to run Ruby with rbenv (https://github.com/rbenv/rbenv)
 
-	`brew install jmeter`
+After installing rbenv, clone this repository and install the ruby version needed:
 
-## Getting the token
+    rbenv install
 
-You will need the survey runner repo for token generation
+Then install bundler (http://bundler.io/):
 
-```
-git clone https://github.com/ONSdigital/eq-survey-runner.git
-cd ./eq-survey-runner/
-python token_generator.py
-```
+    gem install bundler
+
+And install the gems needed by this project:
+
+    bundle install
+
+Finally install jmeter:
+
+	brew install jmeter
 
 ## How to run
 
+    ruby survey_runner_jmeter.rb
+
+
+This opens JMeter with the Ruby generated .jmx file. You're ready to run the tests in JMeter now.
+
+
+## Further Information
+See https://github.com/flood-io/ruby-jmeter
+
+See http://jmeter.apache.org/
+
+### Mapping to JMeter
+`ruby-jmeter` provides a simple mapping between Ruby functions and JMeter XML
+elements so you can add any JMeter XML element by selecting the right Ruby
+function and setting the related properties.
+
+A quick way to do this is to look at the DSL help:
+
+https://github.com/flood-io/ruby-jmeter/blob/master/lib/ruby-jmeter/DSL.md
+
+Which provides a list of the JMeter elements and their Ruby function
+equivalents. For example, to add a JMeter "Random Variable"
+http://jmeter.apache.org/usermanual/component_reference.html#Random_Variable
+
+The DSL.md shows this to be:
+
+`Random Variable random_variable`
+
+Looking at this in the DSL here:
+
+https://github.com/flood-io/ruby-jmeter/blob/master/lib/ruby-jmeter/dsl/random_variable.rb
+
+You can see the various properties that can be set on this in the XML:
+
 ```
-Open JMeter and open Survey-runner.jmx in the application
-Insert the token generated in the decode http request
-Your ready to run the tests
+@doc = Nokogiri::XML(<<-EOS.strip_heredoc)
+<RandomVariableConfig guiclass="TestBeanGUI" testclass="RandomVariableConfig" testname="#{testname}" enabled="true">
+  <stringProp name="maximumValue"/>
+  <stringProp name="minimumValue">1</stringProp>
+  <stringProp name="outputFormat"/>
+  <boolProp name="perThread">false</boolProp>
+  <stringProp name="randomSeed"/>
+  <stringProp name="variableName"/>
+</RandomVariableConfig>)
+      EOS
 ```
+
+e.g. `maximumValue` and `variableName`, so using this in the Ruby script would
+look like:
+
+```
+    random_variable maximumValue: 10000000,
+        minimumValue: 1,
+        perThread: false,
+        variableName: 'ru_ref'
+```
+
+This kind of approach can be used to add any JMeter XML elements to the Ruby
+scripts e.g. `http_request` or `regular_expression_extractor`
+
+See also https://github.com/flood-io/ruby-jmeter/tree/master/examples
---
+++ survey_runner_jmeter.rb
@@ -0,0 +1,173 @@
+require 'rubygems'
+require 'ruby-jmeter'
+
+HOST = 'preprod-surveys.eq.ons.digital'
+PROTOCOL = 'https'
+PORT = '443'
+CONNECT_TIMEOUT_MS = '10000'
+RESPONSE_TIMEOUT_MS = '10000'
+USERS = 300
+REPEAT = 1
+RAMP_UP = 5
+STEP_DELAY_MS = '10000'
+STEP_DELAY_VARIANCE_MS = '5000'
+
+
+def setup_test()
+    defaults domain: HOST,
+        protocol: PROTOCOL,
+        port: PORT,
+        connect_timeout: CONNECT_TIMEOUT_MS,
+        response_timeout: RESPONSE_TIMEOUT_MS,
+        image_parser: false
+end
+
+def initialise_variables()
+    # Use a random number for the ru_ref
+    random_variable maximumValue: 10000000,
+        minimumValue: 1,
+        perThread: false,
+        variableName: 'ru_ref'
+end
+
+def get_healthcheck()
+    visit name: 'status', url: '/status' do
+        assert contains: 'OK'
+    end
+end
+
+def extract_url()
+    extract regex: '(.*)', name: 'url', useHeaders: 'URL'
+end
+
+def start_survey()
+    jwt_params = {
+        user_id: 'ruby-jmeter',
+        schema: '0_star_wars.json',
+        exp: '1800',
+        period_str: 'May 2016',
+        period_id: '201605',
+        collection_exercise_sid: '000',
+        ru_ref: '${ru_ref}',
+        ru_name: 'JMeter',
+        ref_p_start_date: '2016-05-01',
+        ref_p_end_date: '2016-05-31',
+        return_by: '2016-06-12',
+        trad_as: 'Ruby JMeter',
+        employment_date: '2016-06-10',
+        submit: ''
+    }
+
+    # Go to the /dev page and start the questionnaire
+    submit name: 'post dev form', url: '/dev', fill_in: jwt_params do
+        assert contains: ['Star Wars', jwt_params[:ru_name]]
+        extract_url
+    end
+end
+
+def post_introduction()
+    submit name: 'continue introduction', url: '${url}',
+            fill_in: { "action[start_questionnaire]":'' } do
+        assert contains: ['Star Wars Quiz', 'When was The Empire Strikes Back released']
+        extract_url
+    end
+end
+
+def post_page_1_empty()
+    submit name: 'post page 1 (empty)', url: '${url}',
+            fill_in: {
+                "6cf5c72a-c1bf-4d0c-af6c-d0f07bc5b65b":"",
+                "92e49d93-cbdc-4bcb-adb2-0e0af6c9a07c":"",
+                "a5dc09e8-36f2-4bf4-97be-c9e6ca8cbe0d":"",
+                "9587eb9b-f24e-4dc0-ac94-66117b896c10[]":"",
+                "6fd644b0-798e-4a58-a393-a438b32fe637-day":"",
+                "6fd644b0-798e-4a58-a393-a438b32fe637-month":"",
+                "6fd644b0-798e-4a58-a393-a438b32fe637-year":"",
+                "06a6a4b7-6ce4-4687-879d-3443cd8e2ff0-day":"",
+                "06a6a4b7-6ce4-4687-879d-3443cd8e2ff0-month":"",
+                "06a6a4b7-6ce4-4687-879d-3443cd8e2ff0-year":"",
+                "action[save_continue]": "" } do
+        assert contains: ['This field is mandatory.', 'date entered is not valid']
+        extract_url
+    end
+end
+
+def post_page_1_filled()
+    submit name: 'post page 1 (filled)', url: '${url}',
+            fill_in: {
+                "6cf5c72a-c1bf-4d0c-af6c-d0f07bc5b65b":"99",
+                "92e49d93-cbdc-4bcb-adb2-0e0af6c9a07c":"1024",
+                "a5dc09e8-36f2-4bf4-97be-c9e6ca8cbe0d":"Lion",
+                "9587eb9b-f24e-4dc0-ac94-66117b896c10[]":"Yoda",
+                "6fd644b0-798e-4a58-a393-a438b32fe637-day":"1",
+                "6fd644b0-798e-4a58-a393-a438b32fe637-month":"6",
+                "6fd644b0-798e-4a58-a393-a438b32fe637-year":"2015",
+                "06a6a4b7-6ce4-4687-879d-3443cd8e2ff0-day":"1",
+                "06a6a4b7-6ce4-4687-879d-3443cd8e2ff0-month":"5",
+                "06a6a4b7-6ce4-4687-879d-3443cd8e2ff0-year":"2016",
+                "action[save_continue]": ""
+            } do
+        assert contains: ['Ewokes', 'medal']
+        extract_url
+    end
+end
+
+def post_page_2_filled()
+    submit name: 'post page 2 (filled)', url: '${url}',
+            fill_in: {
+                "215015b1-f87c-4740-9fd4-f01f707ef558":"No comment",
+                "7587qe9b-f24e-4dc0-ac94-66118b896c10":"No",
+                "action[save_continue]": ""
+            } do
+        assert contains: 'Your responses'
+        extract_url
+    end
+end
+
+def post_final_submission()
+    submit name: 'post final submission', url: '${url}',
+            fill_in: { "action[submit_answers]": "" } do
+        assert contains: ['Thank You', 'Transaction ID']
+    end
+end
+
+test do
+    setup_test
+
+    threads count: USERS, loops: REPEAT, ramp_time: RAMP_UP do
+
+        initialise_variables
+
+        get_healthcheck
+
+        # Wait a few seconds before continuing
+        think_time 2000
+
+        # Launch the survey, fill in and submit it
+        transaction name: 'Complete Survey' do
+            cookies clear_each_iteration: true
+
+            # Wait between steps a random time
+            random_timer STEP_DELAY_MS, STEP_DELAY_VARIANCE_MS
+
+            start_survey
+
+            post_introduction
+
+            post_page_1_empty
+
+            post_page_1_filled
+
+            post_page_2_filled
+
+            post_final_submission
+        end
+
+        # Show the results in the UI
+        view_results_tree
+        summary_report
+    end
+end.run(
+    file: 'survey_runner_jmeter.jmx',
+    jtl: 'result.jtl',
+    gui: true)
